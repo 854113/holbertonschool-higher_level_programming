b2b563680503ae061a5beef27ccb47499f66443b
854113 <9392@holbertonstudents.com>
2025-09-22 17:30:32 -0300

    update

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Representa un cuadrado."""
+
+    def __init__(self, size):
+        """Inicializa un nuevo cuadrado.
+        
+        Args:
+            size: tamaño del cuadrado (privado).
+        """
+        self.__size = size
