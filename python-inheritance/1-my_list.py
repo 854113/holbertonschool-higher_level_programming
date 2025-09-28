88c829c19444dda1c0265510e36326326a1b8e55
854113 <9392@holbertonstudents.com>
2025-09-28 13:40:09 -0300

    update

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""module containing class called Mylist"""
+"""this module contains a class called Mylist"""
 
 
 class MyList(list):
-    """class MyList that inherits from list"""
+    """a class MyList that inherits from list"""
+
     def print_sorted(self):
         number = list(self)
         number = sorted(number)
