7370e9c1b87471ec72c69a5ba71bfe509da64d51
854113 <9392@holbertonstudents.com>
2025-09-28 13:25:43 -0300

    update

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""This module contains a function"""
+
+
 def is_kind_of_class(obj, a_class):
-    return isinstance(obj, a_class)
+    """bool function that returns True if the object is an instance, or if the
+    object is an instance of a class that inherited from, the specified class,
+    otherwise False"""
+
+    return type(obj) is a_class or issubclass(obj.__class__, a_class)
