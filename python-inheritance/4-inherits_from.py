58e2b8906e8588ca8f0bd08795336089f0a03321
854113 <9392@holbertonstudents.com>
2025-09-28 13:28:47 -0300

    update

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""This module contains a function called is inherits from"""
+
+
 def inherits_from(obj, a_class):
-    return issubclass(obj.__class__, a_class) and obj.__class__ != a_class
+    """function that returns True if the object is an instance of a class
+      that inherited from the specified class, otherwise False"""
+
+    return issubclass(obj.__class__, a_class) and type(obj) is not a_class
---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Script that defines a class called BaseGeometry"""
+
+
+class BaseGeometry:
+    """Represents a class called base geometry"""
+    pass
